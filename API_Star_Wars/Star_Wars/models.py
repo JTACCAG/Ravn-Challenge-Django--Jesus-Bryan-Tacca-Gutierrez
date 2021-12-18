7e2d06fc39c7074b64a6486165de2d2b246c396c
JTACCAG <96223429+JTACCAG@users.noreply.github.com>
2021-12-18 01:35:14 -0500

    Create Model person

--- API_Star_Wars/Star_Wars/models.py
+++ API_Star_Wars/Star_Wars/models.py
@@ -1,3 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
+
+class person(models.Model):
+    Name        = models.CharField(max_length=60)
+    Birth_Year  = models.CharField(max_length=60)
+    Eye_Color   = models.CharField(max_length=60)
+    Gender      = models.CharField(max_length=60)
+    Height      = models.IntegerField()
+    def __str__(self):
+        return self.Name
