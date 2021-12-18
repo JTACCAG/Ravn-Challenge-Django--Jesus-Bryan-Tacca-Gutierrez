a1e2274f692ca0728ed99bf62774ac0085bec80e
JTACCAG <96223429+JTACCAG@users.noreply.github.com>
2021-12-18 01:50:24 -0500

    Display Data

--- API_Star_Wars/Star_Wars/views.py
+++ API_Star_Wars/Star_Wars/views.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
+from .serializers import PersonSerializer
+from .models import Person
 from django.shortcuts import render
 
 # Create your views here.
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all().order_by('Name')
+    serializers_class = PersonSerializer
